d6d99b15b65266f6ff4ee516068b6a3eb14c1cf3
TaipeiGuru <78506727+TaipeiGuru@users.noreply.github.com>
2023-02-15 10:55:16 -0800

    Update hashtable.cpp

--- hashtable.cpp
+++ hashtable.cpp
@@ -9,17 +9,18 @@ void addStudent(Node** myList, int hashFunctionValue, Student* newStudent);
 int hashFunction(int id, int listSize);
 void printStudent(Node** myList, Node* headNode);
 bool collisionChecker(Node** list, int hashFunctionValue);
+void reHash(int listSize, int newListSize, Node** list, Node** temp, int hash, int myID, Student* newStudent);
 // void deleteStudent(Node* &headNode, int myID); 
 
 int main() {
   char input[20];
   bool active = true;
   int listSize = 100;
-  Node** list;
+  Node** list = new Node*[listSize];
   int myID;
   float myGPA;
   int hash;
-  Node* headNode;
+  Node* headNode = NULL;
   
   // Sets each object in the array to null. Help with this from https://stackoverflow.com/questions/4178804/how-to-see-if-an-element-is-null-in-an-array-in-c
   for(int i = 0; i<100; i++) {
@@ -50,30 +51,14 @@ int main() {
       hash = hashFunction(myID, listSize);
 
       if(collisionChecker(list, hash) == true) {
-	Node* temp[listSize];
-	for(int j = 0; j < listSize; j++) {
-	  temp[j] = list[j];
-	}
-	int newListSize = listSize * 2;
-	list = new Node*[newListSize];
+	      int newListSize = listSize * 2;
+	      Node** temp = new Node*[newListSize];
+        
+        for(int j = 0; j < newListSize; j++) {
+          temp[j] = NULL;
+        }
 
-	hash = hashFunction(myID, newListSize);
-	for(int m = 0; m < listSize; m++) {
-	  if(temp[m] != NULL) {
-	    list[m] = temp[m];
-	    if(temp[m]->getNext() != NULL) {
-	      list[m]->setNext(temp[m]->getNext());
-	      if(temp[m]->getNext()->getNext() != NULL) {
-		list[m]->getNext()->setNext(temp[m]->getNext()->getNext());
-	      }
-	    }
-	  } else {
-	    list[m] = NULL;
-	  }
-	}
-	
-	delete[] temp;
-	listSize = newListSize;
+        reHash(listSize, newListSize, list, temp, hash, myID, newStudent);
       }
       addStudent(list, hash, newStudent);
     } else if(strcmp(input, "DELETE") == 0){   
@@ -92,7 +77,6 @@ int main() {
 
 
 void addStudent(Node** myList, int hashFunctionValue, Student* newStudent) {
-  cout << "hi" << endl;
   if(myList[hashFunctionValue] == NULL) {
     Node* tempNode = new Node(newStudent);
     myList[hashFunctionValue] = tempNode;
@@ -105,25 +89,25 @@ void addStudent(Node** myList, int hashFunctionValue, Student* newStudent) {
     Node* tempNode = new Node(newStudent);
     myList[hashFunctionValue]->getNext()->setNext(tempNode);
     cout << "Student added." << endl;
-  } else {
-    int newSize = 2*sizeof(myList)/sizeof(myList[0]);
-    Node* newList[newSize];
+  }  
+}
 
-    for(int j = 0; j<newSize; j++) {
-      if(myList[j] != NULL) {
-	newList[j] = myList[j];
+void reHash(int listSize, int newListSize, Node** list, Node** temp, int hash, int myID, Student* newStudent) {
+  for (int i = 0; i < listSize; i++) {
+    if (list[i] != NULL) {
+      Node* tempNode = list[i];
+      while (tempNode != NULL) {
+        hash = hashFunction(myID, newListSize);
+        addStudent(temp, hash, newStudent);
+        tempNode = tempNode->getNext();
       }
     }
-    
-    delete[] myList;
-    
-    Node* tempNode = new Node(newStudent);
-    int newValue = hashFunction(tempNode->getStudent()->getID(), newSize); 
-    myList[newValue] = tempNode;
-    cout << "Student added." << endl;
-  } 
+  }
+  delete[] &list;
+  list = temp;
+	listSize = newListSize;
 }
-      
+
 int hashFunction(int id, int listSize) {
   int value = id % listSize;
   return value;
@@ -153,7 +137,7 @@ bool collisionChecker(Node** list, int hashFunctionValue) {
   if(list[hashFunctionValue] != NULL) {
     if(list[hashFunctionValue]->getNext() != NULL) {
       if(list[hashFunctionValue]->getNext()->getNext() != NULL) {
-	return true;
+	      return true;
       }
     }
   }
